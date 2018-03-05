828fdf5afb079d04641266efd09662e061f4e56e
ardyd <ardyd@rose-hulman.edu>
2018-03-05 11:35:16 -0500

    no comment

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and David Ardy.
 """
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m4e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -27,3 +27,44 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+tyler1 = rg.SimpleTurtle('turtle')
+tyler1.pen = rg.Pen('red', 5)
+tyler1.speed = 15
+
+imaqtpie = rg.SimpleTurtle('turtle')
+imaqtpie.pen = rg.Pen('blue', 3)
+imaqtpie.speed = 20
+
+sizet1 = 100
+sizeqt = 75
+
+for k in range(15):
+    tyler1.draw_circle(sizet1)
+
+    tyler1.pen_up()
+    tyler1.forward(20)
+    tyler1.left(45)
+    tyler1.pen_down()
+
+    sizet1 = sizet1 - 4
+
+for k in range(13):
+    imaqtpie.draw_square(sizeqt)
+
+    imaqtpie.pen_up()
+    imaqtpie.backward(10)
+    imaqtpie.right(45)
+    imaqtpie.backward(20)
+    imaqtpie.pen_down()
+
+    sizeqt = sizeqt - 3
+
+window.close_on_mouse_click()
+
+
+
